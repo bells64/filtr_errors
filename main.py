9b1fc2030c95de0deb64406514d91b1707735fdd
bells64 <bells64@github.com>
2019-11-29 16:33:57 +0600

    adding src

---
+++ main.py
@@ -0,0 +1,19 @@
+try:
+    print(10)
+except ValueError:
+    print('ValueError')
+except BlockingIOError:
+    print('BlockingIOError')
+except ChildProcessError:
+    print('ChildProcessError')
+except ConnectionError:
+    print('ConnectionError')
+except FileExistsError:
+    print('FileExistsError')
+except OSError:
+    print('OSError')
+except OSError:
+    print('OSError')
+except OSError:
+    print('OSError')
+# ну и.т.д
